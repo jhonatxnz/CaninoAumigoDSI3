d03acf3065450978d562ba934361aae0f02e2806
Jhon <jw.jhonatan1711@gmail.com>
2022-05-26 23:19:02 -0300

    rodape quase certo

--- canino/src/App.js
+++ canino/src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
 import Menu from './components/template/Menu';
+import Rodape from './components/template/Rodape';
 
 function App() {
     return (
         <div className="app">
             <Menu/>
             <p>shgadhjsajhdgajhsdgasdjhgajh</p>
+            
+            <Rodape/>
         </div>
     );
 }
---
+++ canino/src/components/template/Rodape.js
@@ -0,0 +1,38 @@
+import "./Rodape.css"
+import Logo from "./Logo"
+
+export default function Rodape(props) {
+    return (
+        <footer>
+            <div>
+
+                <div>
+                    <Logo/>
+                </div>
+
+                <div>
+                    <h2>Como ajudar?</h2>
+                    <a href="/adotar">Quero adotar</a><br/>
+                    <a href="/doar">Quero doar</a>
+                </div>
+
+                <div>
+                    <h2>Suporte</h2>
+                    <a href="/email">Email</a><br/>
+                    <a href="/contato">Contato</a>
+                </div>
+
+                <div>
+                    <h2>Redes sociais</h2>
+                    <a href="/facebook">Facebook</a><br/>
+                    <a href="/instagram">Instagram</a>
+                </div>
+
+                <div>
+                    <h2>Sobre nós</h2>
+                </div>
+            </div>
+            
+        </footer>
+    );
+}
