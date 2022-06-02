20a65913382cbb81dfac60e801329a68cddcfa71
JHONATAN WILLIAN DOS SANTOS SILVA <u21686@academico.cotuca.unicamp.br>
2022-06-02 11:29:36 -0300

    adicionando animal...

--- canino/src/components/Doar/Doar.js
+++ canino/src/components/Doar/Doar.js
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 import './Doar.css';
 import axios from 'axios';
-
+const urlAPI = "https://localhost:7042/api/animal";
 
 const initialState = {
-  novaDoacao: {
-    porte: ''
-  }
+  novaDoacao:  { nome: '', raca: '', cor: '', idade: 0, descricao: '', genero: '', vacinacao: '', idPorte: 0, idCidade: 0 },
+  lista: []
 }
 
 export default function Doar(props) {
+
   const [novaDoacao, setNovaDoacao] = useState(initialState.novaDoacao)
 
   function atulizarNovaDoacao(event) {
     const { name, value } = event.target
+
+    console.log(name, value)
 
     setNovaDoacao({
       ...novaDoacao,
@@ -26,7 +28,13 @@ export default function Doar(props) {
   function enviarFormulario(event) {
     event.preventDefault();
 
-    axios.post('https://localhost:', novaDoacao)
+    novaDoacao.idCidade = Number(novaDoacao.idCidade)
+    novaDoacao.idPorte = Number(novaDoacao.idPorte)
+    novaDoacao.idade = Number(novaDoacao.idade)
+
+    console.log(novaDoacao)
+
+    axios.post('https://localhost:7042/api/animal', novaDoacao)
   }
 
   return (
@@ -39,42 +47,50 @@ export default function Doar(props) {
           <label>
             Nome:
           </label>
-          <input type="text" name="porte" value={novaDoacao.porte} onChange={atulizarNovaDoacao} />
+          <input type="text" name="nome" value={novaDoacao.nome} onChange={atulizarNovaDoacao} />
           <label>
             Raça:
           </label>
-          <input type="text" name="porte" value={novaDoacao.porte} onChange={atulizarNovaDoacao} />
+          <input type="text" name="raca" value={novaDoacao.raca} onChange={atulizarNovaDoacao} />
           <label>
             Cor:
           </label>
-          <input type="text" name="porte" value={novaDoacao.porte} onChange={atulizarNovaDoacao} />
+          <input type="text" name="cor" value={novaDoacao.cor} onChange={atulizarNovaDoacao} />
           <label>
             Idade:
           </label>
-          <input type="number" name="porte" value={novaDoacao.porte} onChange={atulizarNovaDoacao} />
+          <input type="number" name="idade" value={novaDoacao.idade} onChange={atulizarNovaDoacao} />
           <label>
             Descrição:
           </label>
-          <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Coloque a descrição do seu bichinho"></textarea>
+          <input type="text" name="descricao" placeholder="Coloque a descrição do seu bichinho" value={novaDoacao.descricao} onChange={atulizarNovaDoacao} />
           <label>
             Gênero:
           </label>
-          <input type="text" name="porte" value={novaDoacao.porte} onChange={atulizarNovaDoacao} />
+          <input type="text" name="genero" value={novaDoacao.genero} onChange={atulizarNovaDoacao} />
           <label>
             Informações cruciais:
           </label>
-          <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Ex:vacinação"></textarea>
+          <input type="text" name="vacinacao"  placeholder="Ex:vacinação" value={novaDoacao.vacinacao} onChange={atulizarNovaDoacao} />
           <label>
             Porte:
           </label>
-          <select name="cars" id="cars">
-            <option value="Grande">Grande</option>
-            <option value="Medio">Médio</option>
-            <option value="Pequeno">Pequeno</option>
+          <select name="idPorte" onChange={atulizarNovaDoacao} value={novaDoacao.idPorte}>
+            <option value="1">Grande</option>
+            <option value="2">Médio</option>
+            <option value="3">Pequeno</option>
           </select>
-          <button>
-                    Salvar
-                </button>
+          <label>
+            Cidade:
+          </label>
+          <select name="idCidade" onChange={atulizarNovaDoacao} value={novaDoacao.idCidade}>
+            <option value="1">Sumare</option>
+            <option value="2">Campinas</option>
+          </select>
+          <button className="btnSalvar"
+            onClick={e => enviarFormulario(e)} >
+            Salvar
+          </button>
         </form>
       </main>
     </div>
